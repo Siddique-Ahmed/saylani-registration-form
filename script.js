74f3781bef5c2691339716bb8402ed77089dcbd3
Siddique-Ahmed <itssiddique786@gmail.com>
2024-05-10 10:03:00 -0700

    change in submit button for checking

---
+++ script.js
@@ -0,0 +1,6 @@
+let submitBtn = document.querySelector("#SubmitBtn");
+let submitForm = document.querySelector("#submit-form");
+
+submitBtn.addEventListener("click", ()=>{
+  submitForm.style.display = "flex"
+})
