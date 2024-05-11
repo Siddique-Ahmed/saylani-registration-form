5b6a3c6f018d457a597687971b2a4009f182c692
Siddique-Ahmed <itssiddique786@gmail.com>
2024-05-11 10:06:19 -0700

    localstorage work

--- script.js
+++ script.js
@@ -1,43 +1,92 @@
 // ###############3 getting all input and select ############### \\
 
-let city = document.querySelector("#city option");
-let course = document.querySelector("#course option");
-let campus = document.querySelector("#campus option");
-let classOption = document.querySelector("#class option");
-let gender = document.querySelector("#gender option");
-let qualification = document.querySelector("#qualification option");
-let laptop = document.querySelector("#laptop option");
+let city = document.querySelector("#city");
+let course = document.querySelector("#course");
+let campus = document.querySelector("#campus");
+let classOption = document.querySelector("#class");
+let gender = document.querySelector("#gender");
+let qualification = document.querySelector("#qualification");
+let laptop = document.querySelector("#laptop");
 let userName = document.querySelector("#userName");
 let fatherName = document.querySelector("#fatherName");
 let Email = document.querySelector("#Email");
 let phone = document.querySelector("#phone");
 let CNIC = document.querySelector("#CNIC");
 let fatherCnic = document.querySelector("#fatherCnic");
-let date = document.querySelector("#date");
+let dob = document.querySelector("#date");
 let address = document.querySelector("#address");
+let userPic = document.querySelector("#userPicture");
 let submitBtn = document.querySelector("#SubmitBtn");
 let submitForm = document.querySelector("#submit-form");
 
-function getRegistrationData() {
-  city = city.value;
-  course = course.value;
-  campus = campus.value;
-  classOption = classOption.value;
-  gender = gender.value;
-  qualification = qualification.value;
-  laptop = laptop.value;
-  userName = userName.value;
-  fatherName = fatherName.value;
-  Email = Email.value;
-  phone = phone.value;
-  CNIC = CNIC.value;
-  fatherCnic = fatherCnic.value;
-  date = date.value;
-  address = address.value;
+function getStudentData() {
+  let cityValue = city.value;
+  let courseValue = course.value;
+  let campusValue = campus.value;
+  let classOptionValue = classOption.value;
+  let genderValue = gender.value;
+  let qualificationValue = qualification.value;
+  let laptopValue = laptop.value;
+  let fatherNameValue = fatherName.value;
+  let userNameValue = userName.value;
+  let EmailValue = Email.value;
+  let phoneValue = phone.value;
+  let CNICValue = CNIC.value;
+  let fatherCnicValue = fatherCnic.value;
+  let dobValue = dob.value;
+  let addressValue = address.value;
+  let userPicValue = userPic.value;
 
-  submitBtn.addEventListener("click",()=>{
-    submitForm.style.display = "flex";
-  })
+  return {
+    cityValue,
+    courseValue,
+    campusValue,
+    classOptionValue,
+    genderValue,
+    qualificationValue,
+    laptopValue,
+    fatherNameValue,
+    userNameValue,
+    EmailValue,
+    phoneValue,
+    CNICValue,
+    fatherCnicValue,
+    dobValue,
+    addressValue,
+    userPicValue,
+  }
+}
+
+function userDataValidation(){
 
 }
-getRegistrationData();
+
+function GettingData() {
+  // Getting getStudentData funtion to save data in localStorage //
+  let studentData = getStudentData();
+
+  // saving data in localStorage //
+  let getCityValue = localStorage.setItem("cityValue", studentData.cityValue)
+  let getCourseValue = localStorage.setItem("courseValue", studentData.courseValue)
+  let getCampusValue = localStorage.setItem("campusValue", studentData.campusValue)
+  let getClassOptionValue = localStorage.setItem("classOptionValue", studentData.classOptionValue)
+  let getGenderValue = localStorage.setItem("genderValue", studentData.genderValue)
+  let getQualificationValue = localStorage.setItem("qualificationValue", studentData.qualificationValue)
+  let getLaptopValue = localStorage.setItem("laptopValue", studentData.laptopValue)
+  let getFatherNameValue = localStorage.setItem("fatherNameValue", studentData.fatherNameValue)
+  let getUserNameValue = localStorage.setItem("userNameValue", studentData.userNameValue)
+  let getEmailValue = localStorage.setItem("EmailValue", studentData.EmailValue)
+  let getPhoneValue = localStorage.setItem("phoneValue", studentData.phoneValue)
+  let getCNICValue = localStorage.setItem("CNICValue", studentData.CNICValue)
+  let getFatherCnicValue = localStorage.setItem("fatherCnicValue", studentData.fatherCnicValue)
+  let getDobValue = localStorage.setItem("dobValue", studentData.dobValue)
+  let getAddressValue = localStorage.setItem("addressValue", studentData.addressValue)
+  let getUSerPicValue = localStorage.setItem("userPicValue", studentData.userPicValue)
+}
+
+// ########### submit button ############# \\
+
+submitBtn.addEventListener("click", () => {
+  GettingData()
+  submitForm.style.display = "flex";
+});
