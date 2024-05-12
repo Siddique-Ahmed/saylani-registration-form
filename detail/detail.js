c16bfd0b5b5922beac3250ae4b50f5bc344b2022
Siddique-Ahmed <itssiddique786@gmail.com>
2024-05-12 06:00:39 -0700

    try to get data from another page

---
+++ detail/detail.js
@@ -0,0 +1,13 @@
+let userImg = document.querySelector("#userImg");
+let userName = document.querySelector("#userName");
+let userCourse = document.querySelector("#userCourse");
+let userRollNumber = document.querySelector("#userRollNumber");
+let username = document.querySelector("#username");
+let fatherName = document.querySelector("#fatherName");
+let cnic = document.querySelector("#cnic");
+
+
+
+  let getName = localStorage.getItem("userPicValue");
+
+  console.log(getName);
